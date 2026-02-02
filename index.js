2eeb87384398adcdfc08f4949d366d67df522952
Andrey Zuev <a.zuev@texrus.com>
2026-02-02 20:27:38 +1000

    Added catch error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Bot } = require('grammy');
+const { Bot, GrammyError, HttpError } = require('grammy');
 const bot = new Bot(process.env.TOKEN_BOT);
 //
 bot.command('start', async (ctx) => {
@@ -8,6 +8,19 @@ bot.command('start', async (ctx) => {
 //
 bot.on('message', async (ctx) => {
   await ctx.reply('I need to think about it.');
+});
+
+bot.catch((err) => {
+  const { ctx } = err;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const { error } = err;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', error.description);
+  } else if (error instanceof HttpError) {
+    console.error('Could not contact Telegram:', error);
+  } else {
+    console.error('Unknown error:', error);
+  }
 });
 //
 bot.start();
